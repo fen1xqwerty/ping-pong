4cdabd017c86c1f4a77bbbfb63108a267195804f
Ping-Pong <showtiger325@gmail.com>
2023-11-05 16:28:32 +0400

    2 этап

--- ping-pong.py
+++ ping-pong.py
@@ -1,12 +1,30 @@
 from pygame import*
 
+
+
+
+class Racket(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
 win_width = 700
 win_height = 500
 display.set_caption("Ping-pong")
 window = display.set_mode((win_width, win_height))
 back = (200, 255, 255)
 window.fill(back)
-
 
 
 game = True
