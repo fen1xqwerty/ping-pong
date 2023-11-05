5c97e2cf5508f69cbb3f7e6f683e27c85b8d0a94
Ping-Pong <showtiger325@gmail.com>
2023-11-05 15:58:52 +0400

    сделал 0 этап

--- ping-pong.py
+++ ping-pong.py
@@ -1,1 +1,17 @@
-import pygame 
+from pygame import*
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping-pong")
+window = display.set_mode((win_width, win_height))
+back = (200, 255, 255)
+window.fill(back)
+
+
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+           game = False
+    display.update()
